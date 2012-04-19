42e1df34295432295f6b0e4778382ee951669da8
zhenya <zhenya@laptop>
2012-04-20 01:14:48 +0300

    Added new method for searching phrases

--- src/main/java/com/iusenko/subrip/PhraseUtils.java
+++ src/main/java/com/iusenko/subrip/PhraseUtils.java
@@ -13,4 +13,32 @@ public class PhraseUtils {
 		}
 		return null;
 	}
+
+	public static Phrase find(long start, long end, List<Phrase> phrases) {
+		StringBuilder sb = new StringBuilder();
+		int num = 0;
+		Phrase phrase = new Phrase();
+		for (Phrase p : phrases) {
+			boolean a = p.startTime <= start && start < p.endTime;
+			boolean b = start <= p.startTime && p.endTime <= end;
+			// boolean c = phrase.startTime < end && end <= phrase.endTime;
+
+			if (b || a) {
+				phrase.endTime = p.endTime;
+				phrase.endTimeText = p.endTimeText;
+
+				if (num == 0) {
+					phrase.startTime = p.startTime;
+					phrase.startTimeText = p.startTimeText;
+					num++;
+				}
+
+				sb.append(p.text).append("\n\n");
+			}
+		}
+		phrase.text = sb.toString().trim();
+
+		return phrase;
+	}
+
 }
